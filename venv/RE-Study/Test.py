3a2dbe693da08cb554e94d58e3622f04dc259173
EternalYG <1050359542@qq.com>
2020-04-17 15:59:21 +0800

    417提交

--- venv/RE-Study/Test.py
+++ venv/RE-Study/Test.py
@@ -1,3 +1,18 @@
 #coding=utf-8
 #正则表达式实例
 
+import  re
+
+p1 = r'\w+@qq\.com'
+p2 = r'^\w+@qq\.com$'
+
+text = "Yang's email is yang_123@qq.com."
+m1 = re.search(p1, text)
+print(m1)
+
+m2 = re.search(p2, text)
+print(m2)
+
+email =  'yang_123@qq.com'
+m3 = re.search(p2, email)
+print(m3)
